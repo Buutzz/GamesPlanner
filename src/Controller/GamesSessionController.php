88c76ae619fc2937dca2b5dbb6fa5372b5fe4923
Buc <wojdala.mateusz@gmail.com>
2026-05-01 18:35:20 +0200

    New API endopoint for Discord Bot

--- src/Controller/GamesSessionController.php
+++ src/Controller/GamesSessionController.php
@@ -11,10 +11,16 @@ use App\Repository\GameSessionRepository;
 #[Route('/api')]
 class GamesSessionController extends AbstractController
 {
+
+    private $gameSessionRepository;
+    
+    public function __construct(GameSessionRepository $gameSessionRepository) {
+        $this->gameSessionRepository = $gameSessionRepository;
+    }
+
     #[Route('/sessions/upcoming', methods: ['GET'], name:'api_sessions_upcoming')]
     public function upcoming(
         Request $request,
-        GameSessionRepository $repo
     ): JsonResponse {
 
         $token = $request->headers->get('X-API-TOKEN');
@@ -25,7 +31,7 @@ class GamesSessionController extends AbstractController
         $start = new \DateTimeImmutable((new \DateTime())->format('Y-m-d 00:00:01'));
         $end = $start->modify('last day of +1 month')->setTime(23, 59, 59);
 
-        $sessions = $repo->findUpcoming($start, $end);
+        $sessions = $this->gameSessionRepository->findUpcoming($start, $end);
 
         $data = [];
 
@@ -43,5 +49,33 @@ class GamesSessionController extends AbstractController
         }
 
         return $this->json($data);
+    }
+
+    #[Route('/games/active', methods: ['GET'], name:'api_sessions_upcoming')]
+    public function getListOfActiveGamesWithSessions(): JsonResponse {
+        $sessions = $this->gameSessionRepository->findActiveSessions();
+
+        $data = [];
+
+        foreach ($sessions as $session) {
+            $game = $session->getGame();
+
+            if (!isset($data[$game->getId()])) {
+                $data[$game->getId()] = [
+                    'id' => $game->getId(),
+                    'name' => $game->getName(),
+                    'sessions' => []
+                ];
+            }
+
+            $data[$game->getId()]['sessions'][] = [
+                'id' => $session->getId(),
+                'date' => $session->getDate()->format('Y-m-d'),
+                'time' => $session->getSessionStartingTime()?->format('H:i') ?? "19:30",
+            ];
+        }
+
+        return $this->json(array_values($data));
+
     }
 }
